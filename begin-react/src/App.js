62edab4f14f2b3642ab227535d4f535bf61d643e
hwimin-kim <gnlals7841@gmail.com>
2022-07-10 00:07:11 +0900

    useState 동적 값 관리

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,17 +1,15 @@
+import Counter from './Conuter';
 import Hello from "./Hello";
 import Wrapper from "./Wrapper";
 
 function App() {
   return (
-    <Wrapper>
-      <Hello color="aqua"></Hello>
-      <Hello name="react" color="red" isSpecial />
-    </Wrapper>
+    <Counter></Counter>
   );
 }
 
-Hello.defaultProps = {
-  name: "이름없음",
-};
+// Hello.defaultProps = {
+//   name: "이름없음",
+// };
 
 export default App;
