5db058d685b5a6a480bd6404e0ee40fb59c1459d
hwimin-kim <gnlals7841@gmail.com>
2022-07-10 18:29:15 +0900

    배열 항목 추가

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,9 +1,24 @@
-import React, {useRef} from 'react';
+import React, {useState, useRef} from 'react';
+import CreateUser from './CreateUser';
 import UserList from './UserList';
 
 
 function App() {
-  const users = [
+  const [inputs, setInputs] = useState({
+    username:'',
+    email:''
+  })
+  const {username, email} = inputs;
+
+  const onChange = (e) => {
+  const {name, value} = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value
+    })
+  };
+
+  const [users,setUsers] = useState([
     {
         id: 1,
         username: 'hwimin',
@@ -19,17 +34,36 @@ function App() {
         username: 'barth',
         email: 'barth@gmail.com'
     },
-  ];
+  ]);
   
   const nextId = useRef(4);
 
   const onCreate = () => {
-    console.log(nextId.current);
+    const user = {
+      id: nextId.current,
+      username,
+      email
+    };
+    setUsers(users.concat(user));
+    //setUsers([...users, user]);
+
+    setInputs({
+      username:'',
+      email:''
+    })
     nextId.current += 1;
   };
 
   return (
-    <UserList users={users} />
+    <>
+      <CreateUser 
+        username={username} 
+        email={email} 
+        onChange={onChange} 
+        onCreate={onCreate} 
+      />
+      <UserList users={users} />
+    </>
   );
 }
 
