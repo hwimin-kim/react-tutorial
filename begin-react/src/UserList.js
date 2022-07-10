fda55ae983dfcfe881f86a06a8867ffbd23c9699
hwimin-kim <gnlals7841@gmail.com>
2022-07-10 18:49:00 +0900

    배열 삭제

--- begin-react/src/UserList.js
+++ begin-react/src/UserList.js
@@ -1,19 +1,26 @@
 import React from 'react';
 
-function User({user}) {
+function User({user, onRemove}) {
+    const {id, username, email} = user
     return (
         <div>
-            <b>{user.username}</b>
-            <span>({user.email})</span>
+            <b>{username}</b>
+            <span>({email})</span>
+            <button onClick={()=>onRemove(id)}>삭제</button>
         </div>
     )
 }
 
-function UserList({users}) {
+function UserList({users, onRemove}) {
     return (
         <div>
             {users.map(
-                user =><User user={user} key={user.id} />
+                user =>
+                    <User 
+                        user={user} 
+                        key={user.id} 
+                        onRemove={onRemove} 
+                    />
                 // Array.map은 reRendering시 성능 향상을 위해
                 // 고유한 key를 주어야 한다.
                 // key가 없을 경우 map 함수의 두번째 인자인 index를 사용.
