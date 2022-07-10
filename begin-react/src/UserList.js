1e591db0eea6c8a3abb7a26bc9ea66dd5edbc5c7
hwimin-kim <gnlals7841@gmail.com>
2022-07-10 17:42:55 +0900

    Array map을 이용한 배열 랜더링

---
+++ begin-react/src/UserList.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+function User({user}) {
+    return (
+        <div>
+            <b>{user.username}</b>
+            <span>({user.email})</span>
+        </div>
+    )
+}
+
+function UserList() {
+    const users = [
+        {
+            id: 1,
+            username: 'hwimin',
+            email: 'gnlals@gmail.com'
+        },
+        {
+            id: 2,
+            username: 'homer',
+            email: 'homer@gmail.com'
+        },
+        {
+            id: 3,
+            username: 'barth',
+            email: 'barth@gmail.com'
+        },
+    ];
+
+    return (
+        <div>
+            {users.map(
+                user =><User user={user} key={user.id} />
+                // Array.map은 reRendering시 성능 향상을 위해
+                // 고유한 key를 주어야 한다.
+                // key가 없을 경우 map 함수의 두번째 인자인 index를 사용.
+                // (user, index) =><User user={user} key={index} />
+                // 하지만 index 사용 시 에러는 사라지지만 성능 향상은 없다.
+                )
+            }
+        </div>
+    )
+}
+
+export default UserList;
