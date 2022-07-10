f053dcf416c452e1555f74f337e3100fd7dc89d0
hwimin-kim <gnlals7841@gmail.com>
2022-07-10 23:32:08 +0900

    useEffect

--- begin-react/src/UserList.js
+++ begin-react/src/UserList.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
 function User({user, onRemove, onToggle}) {
-    const {id, username, email, active} = user
+    const {id, username, email, active} = user;
+
+    useEffect(()=> {
+        console.log('컴포넌트가 화면에 나타남');
+        return () => {
+            console.log('컴포넌트가 화면에 사라짐');
+        }
+    }, []);
+
     return (
         <div>
             <b 
@@ -22,12 +30,12 @@ function UserList({users, onRemove, onToggle}) {
         <div>
             {users.map(
                 user =>
-                    <User 
+                    (<User 
                         user={user} 
                         key={user.id} 
                         onRemove={onRemove} 
                         onToggle={onToggle}
-                    />
+                    />)
                 // Array.map은 reRendering시 성능 향상을 위해
                 // 고유한 key를 주어야 한다.
                 // key가 없을 경우 map 함수의 두번째 인자인 index를 사용.
